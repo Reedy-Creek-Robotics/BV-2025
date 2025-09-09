6f591a41492370358a19af0205deb4128247365c
derickgithub <danielns@gmail.com>
2025-09-08 21:49:38 -0400

    Adding basic driving teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp(name = "TeleOP Drive")
+public class TeleOpDrive extends LinearOpMode {
+    DcMotor driveFrontLeft, driveFrontRight, driveBackRight, driveBackLeft;
+    IMU imu;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initHardware();
+        waitForStart();
+        while(opModeIsActive()){
+            driveControl();
+        }
+
+    }
+    private void initHardware(){
+        driveFrontLeft = hardwareMap.get(DcMotor.class, "driveFrontLeft");
+        driveFrontLeft.setMode(STOP_AND_RESET_ENCODER);
+        driveFrontLeft.setMode(RUN_USING_ENCODER);
+        driveFrontLeft.setZeroPowerBehavior(BRAKE);
+        driveFrontLeft.setDirection(REVERSE);
+
+        driveFrontRight = hardwareMap.get(DcMotor.class, "driveFrontRight");
+        driveFrontRight.setMode(STOP_AND_RESET_ENCODER);
+        driveFrontRight.setMode(RUN_USING_ENCODER);
+        driveFrontRight.setZeroPowerBehavior(BRAKE);
+
+        driveBackLeft = hardwareMap.get(DcMotor.class, "driveBackLeft");
+        driveBackLeft.setMode(STOP_AND_RESET_ENCODER);
+        driveBackLeft.setMode(RUN_USING_ENCODER);
+        driveBackLeft.setZeroPowerBehavior(BRAKE);
+        driveBackLeft.setDirection(REVERSE);
+
+        driveBackRight = hardwareMap.get(DcMotor.class, "driveBackRight");
+        driveBackRight.setMode(STOP_AND_RESET_ENCODER);
+        driveBackRight.setMode(RUN_USING_ENCODER);
+        driveBackRight.setZeroPowerBehavior(BRAKE);
+
+
+        // IMU TODO: replace this with the pinpoint sensor IMU later.
+         imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+    }
+    private void driveControl(){
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        // This button choice was made so that it is hard to hit on accident,
+        // it can be freely changed based on preference.
+        // The equivalent button is start on Xbox-style controllers.
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        driveFrontLeft.setPower(frontLeftPower);
+        driveBackLeft.setPower(backLeftPower);
+        driveFrontRight.setPower(frontRightPower);
+        driveBackRight.setPower(backRightPower);
+    }
+}
