53fe3fc02930e2f2d6e509379d3a01c3d810e05e
derickgithub <danielns@gmail.com>
2025-10-02 19:55:30 -0400

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,225 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Autonomous(name = "Auto with camera")
+public class Auto extends LinearOpMode{
+    private final Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    private final YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+    DcMotor driveFrontLeft, driveFrontRight, driveBackRight, driveBackLeft, outtake;
+    Servo outtakeHammer, transfer;
+    IMU imu;
+    AprilTagProcessor aprilTag;
+    private BuiltinCameraDirection webcamName ;
+    private VisionPortal visionportal;
+
+    public static String MOTIFPATTERN;
+
+    //PP
+    private Follower follower;
+    private int pathState;
+    static Pose endPose;
+
+    private Pose initPos;
+    public final Pose scorePose = new Pose(60, 85, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private final Pose pickup1Pose = new Pose(19.000, 84.000, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
+    private final Pose pickup2Pose = new Pose(19.000, 60,Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
+    private final Pose pickup3Pose = new Pose(19.000, 36, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
+
+    private PathChain grabPickup1,  grabPickup2;
+
+    // possible states for Auto
+    int autoState = 1;
+
+    String artifactOrder = "PPG";
+    double
+            intake1 = (double) 0 /300,
+            intake2 = (double) 120 /300,
+            intake3 = (double) 240 /300;
+            
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initHardware();
+        setApriltag();
+        List results= AprilTagDetection();
+        initPos = (Pose) results.get(1);
+        MOTIFPATTERN = results.get(0).toString();
+        visionportal.close();
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(initPos);
+        buildPaths();
+        waitForStart();
+        transfer.setPosition(0);
+        while(opModeIsActive()){
+            follower.update();
+            switch (autoState){
+                case 1:
+                    follower.followPath(grabPickup1);
+                    autoState++;
+                    break;
+                case 2:
+                    if(!follower.isBusy()) {
+                        score();
+                        follower.followPath(grabPickup2);
+                        autoState++;
+                    }
+                    break;
+                case 3:
+                    if(!follower.isBusy()){
+                        score();
+                        autoState=-1;
+                    }
+                    break;
+
+            }
+        endPose=follower.getPose();
+
+
+        }
+
+
+    }
+
+    private void score() {
+       outtake.setPower(1);
+       if(Objects.equals(MOTIFPATTERN, "PPG")){
+
+       }
+       
+    }
+
+    private void buildPaths() {
+        grabPickup1 = follower.pathBuilder()
+                .addPath( new BezierLine(follower::getPose, new Pose(40.5, 84)))
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                .addPoseCallback(new Pose(40.5,84), this::runIntake, .9)
+                .addPath( new BezierLine(follower::getPose, pickup1Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .addPath(new BezierLine(follower::getPose, scorePose))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                .build();
+
+        grabPickup2 = follower.pathBuilder()
+                .addPath(new BezierLine(follower::getPose, new Pose(40.5,60)))
+                .setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
+                .addPoseCallback(new Pose(40.5,60), this::runIntake, .9)
+                .addPath(new BezierLine(follower::getPose, pickup2Pose))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .addPath(new BezierLine(follower::getPose, scorePose))
+                .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(135))
+                .build();
+    }
+
+    private void runIntake() {
+        // TODO do intake stuff
+    }
+
+    private void initHardware() {
+        transfer = hardwareMap.get(Servo.class, "transfer");
+        outtakeHammer = hardwareMap.get(Servo.class, "outtakeHammer");
+
+        outtake = hardwareMap.get(DcMotor.class, "outtake");
+        outtake.setMode(RUN_WITHOUT_ENCODER);
+        outtake.setZeroPowerBehavior(BRAKE);
+
+
+
+        driveFrontLeft = hardwareMap.get(DcMotor.class, "driveFrontLeft");
+        driveFrontLeft.setMode(STOP_AND_RESET_ENCODER);
+        driveFrontLeft.setMode(RUN_WITHOUT_ENCODER);
+        driveFrontLeft.setZeroPowerBehavior(BRAKE);
+        driveFrontLeft.setDirection(REVERSE);
+
+        driveFrontRight = hardwareMap.get(DcMotor.class, "driveFrontRight");
+        driveFrontRight.setMode(STOP_AND_RESET_ENCODER);
+        driveFrontRight.setMode(RUN_WITHOUT_ENCODER);
+        driveFrontRight.setZeroPowerBehavior(BRAKE);
+
+        driveBackLeft = hardwareMap.get(DcMotor.class, "driveBackLeft");
+        driveBackLeft.setMode(STOP_AND_RESET_ENCODER);
+        driveBackLeft.setMode(RUN_WITHOUT_ENCODER);
+        driveBackLeft.setZeroPowerBehavior(BRAKE);
+        driveBackLeft.setDirection(REVERSE);
+
+        driveBackRight = hardwareMap.get(DcMotor.class, "driveBackRight");
+        driveBackRight.setMode(STOP_AND_RESET_ENCODER);
+        driveBackRight.setMode(RUN_WITHOUT_ENCODER);
+        driveBackRight.setZeroPowerBehavior(BRAKE);
+
+
+
+    }
+
+    private void setApriltag(){
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                .setLensIntrinsics(237.835, 237.835, 328.272, 237.727)
+                // ... these parameters are fx, fy, cx, cy.
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag);
+        visionportal = builder.build();
+    }
+
+    private List AprilTagDetection(){
+        String motif = "unfound";
+        Pose position = null;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                // Only use tags that don't have Obelisk in them
+                if (!detection.metadata.name.contains("Obelisk")) {
+                    position = new Pose(detection.robotPose.getPosition().x,
+                            detection.robotPose.getPosition().y,
+                            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES),
+                            FTCCoordinates.INSTANCE).getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+                }else{
+                    motif = detection.metadata.name.replace("Obelisk-","");
+                }
+
+        }
+        return Arrays.asList(motif, position);
+
+    }
+        return java.util.Collections.emptyList();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -1,36 +1,78 @@
 package org.firstinspires.ftc.teamcode;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.Arrays;
+import java.util.List;
 
 @TeleOp(name = "TeleOP Drive")
 public class TeleOpDrive extends LinearOpMode {
+    private final Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    private final YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
     DcMotor driveFrontLeft, driveFrontRight, driveBackRight, driveBackLeft;
+
+    Servo transfer, outtakeHammer;
     IMU imu;
+    private Follower follower;
+    PathChain score;
+    ElapsedTime buttonDebounce;
+    Gamepad gamepad1 = new Gamepad();
+
+    AprilTagProcessor aprilTag;
+    private VisionPortal visionportal;
+
+    private String MOTIFPATTERN;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
+        MOTIFPATTERN = Auto.MOTIFPATTERN;
+        buttonDebounce = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         initHardware();
+        setApriltag();
+        List apriltagDetections = AprilTagDetection();
+        Pose initPose = (Pose) apriltagDetections.get(1);
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(initPose);
+        createPaths();
         waitForStart();
         while(opModeIsActive()){
-            driveControl();
+            processControl();
         }
 
     }
     private void initHardware(){
-        driveFrontLeft = hardwareMap.get(DcMotor.class, "driveFrontLeft");
+        driveFrontLeft = hardwareMap.get(DcMotor.class,  "driveFrontLeft");
         driveFrontLeft.setMode(STOP_AND_RESET_ENCODER);
         driveFrontLeft.setMode(RUN_USING_ENCODER);
         driveFrontLeft.setZeroPowerBehavior(BRAKE);
@@ -52,6 +94,8 @@ public class TeleOpDrive extends LinearOpMode {
         driveBackRight.setMode(RUN_USING_ENCODER);
         driveBackRight.setZeroPowerBehavior(BRAKE);
 
+        outtakeHammer = hardwareMap.get(Servo.class, "outtakeHammer");
+        transfer = hardwareMap.get(Servo.class, "transfer");
 
         // IMU TODO: replace this with the pinpoint sensor IMU later.
          imu = hardwareMap.get(IMU.class, "imu");
@@ -61,8 +105,9 @@ public class TeleOpDrive extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
+
     }
-    private void driveControl(){
+    private void processControl(){
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
         double x = gamepad1.left_stick_x;
         double rx = gamepad1.right_stick_x;
@@ -95,5 +140,59 @@ public class TeleOpDrive extends LinearOpMode {
         driveBackLeft.setPower(backLeftPower);
         driveFrontRight.setPower(frontRightPower);
         driveBackRight.setPower(backRightPower);
+
+        // now do buttons
+        if(gamepad1.x && buttonDebounce.milliseconds()>250){
+            buttonDebounce.reset();
+            follower.followPath(score);
+        }
+
+
+    }
+
+    private void createPaths(){
+        score = follower.pathBuilder()
+                .addPath(new BezierLine(follower::getPose, new Pose(60, 85)))
+                .setLinearHeadingInterpolation(follower.getHeading(), Math.toRadians(135))
+                .build();
+    }
+    private void setApriltag(){
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                .setLensIntrinsics(237.835, 237.835, 328.272, 237.727)
+                // ... these parameters are fx, fy, cx, cy.
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag);
+        visionportal = builder.build();
+    }
+
+    private List AprilTagDetection(){
+        String motif = "unfound";
+        Pose position = null;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                // Only use tags that don't have Obelisk in them
+                if (!detection.metadata.name.contains("Obelisk")) {
+                    position = new Pose(detection.robotPose.getPosition().x,
+                            detection.robotPose.getPosition().y,
+                            detection.robotPose.getOrientation().getYaw(AngleUnit.DEGREES),
+                            FTCCoordinates.INSTANCE).getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+                }else{
+                    motif = detection.metadata.name.replace("Obelisk-","");
+                }
+
+            }
+            return Arrays.asList(motif, position);
+
+        }
+        return java.util.Collections.emptyList();
     }
 }
